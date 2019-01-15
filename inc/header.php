8612c78ce9d89fa64d9d6fe247c2448c9a1f532f
mathflk <math.delp76@gmail.com>
2019-01-15 14:33:02 +0100

    blblbblblbl

--- inc/header.php
+++ inc/header.php
@@ -1,8 +1,8 @@
 <header class="header">
-    <a href="#default" class="logo">CompanyLogo</a>
+    <a href="index.php" class="logo">TRAME-WAY</a>
     <div class="header-right">
-        <a class="active" href="#home">Home</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <a class="active" href="index.php">Acceuil</a>
+        <a href="fichier.php">Fichier</a>
+        <a href="capture.php">Capture temps réel</a>
     </div>
 </header>
