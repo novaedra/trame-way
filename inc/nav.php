0295c05983fb7398954498498ca09ca5f6392e57
Novaedra <leofouquier20111999@gmail.com>
2019-01-15 14:55:19 +0100

    tout

---
+++ inc/nav.php
@@ -0,0 +1,8 @@
+<nav class="header">
+    <a href="index.php" class="logo">TRAME-WAY</a>
+    <div class="header-right">
+        <a class="active" href="index.php">Accueil</a>
+        <a href="fichier.php">Analyse</a>
+        <a href="capture.php">Capture</a>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -5,18 +5,20 @@
     <title>Trame-Way</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300" type="text/css" />
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 </head>
 <body>
 
 <?php
 include ('function.php');
 include ('inc/header.php');
+include ('inc/nav.php');
 ?>
 
 <div class="container">
-    <div class="fichier"><a class="bloc_lien" href="fichier.php" title="Analyser à partir d'un fichier"><img src="inc/img/file.png" alt="fichier"><br/><p class="option">Analyser des fichiers</p></a></div>
+    <div class="fichier"><a class="bloc_lien" href="fichier.php" title="Analyser à partir d'un fichier"><img src="inc/img/file.png" alt="fichier"><br/><p class="option">Analyser des fichiers JSON</p></a></div>
     <div class="capture"><a class="bloc_lien" href="capture.php" title="Effectuer une capture"><img src="inc/img/network.png" alt="réseau"><br/><p class="option">Commencer une capture</p></a></div>
 </div>
 <div class="clear"></div>
-</body>
-</html>
+
+<?php include ('inc/footer.php'); ?>
