56f399f81fd317c6e85816f531ab97a8adcaf3a6
mathflk <math.delp76@gmail.com>
2019-01-17 17:07:00 +0100

    tttt

--- reseau.php
+++ reseau.php
@@ -4,17 +4,10 @@ include ('inc/function.php');
 include ('inc/header.php');
 include ('inc/nav.php');
 
-$sql = "SELECT nom_resau,ip_saisie,mask,ip_low,ip_high,created_at FROM trame_waygit ";
-
-$req = mysqli_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysqli_error());
-while($data = mysqli_fetch_assoc($req))
-{
-    //
-    echo '<b>'.$data['nom_reseau'].' '.$data['ip_saisie'].'</b> '.$data['mask'].' '.$data['created_at'].;
-    echo ' <i> '.$data['ip_low'].' '.$data['ip_high'].'</i><br>';
+$sql = "SELECT nom_resau, ip_saisie, mask, ip_low, ip_high, created_at FROM trame_way ";
+$query = $pdo -> prepare($sql);
+$return = $query -> fetchAll();
 }
+tab($return);
 
 
-mysqli_close();
-
-echo $row[''];
