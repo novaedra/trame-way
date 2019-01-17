55da89f7dedd627ff1748ea59474357438ce7abb
Novaedra <leofouquier20111999@gmail.com>
2019-01-17 19:26:49 +0100

    fichier

--- reseau.php
+++ reseau.php
@@ -11,8 +11,11 @@ $return = $query -> fetchALL();
 
 tab($return);
 
-echo '<form method="post">';
+echo '<form action="reseau.php" method="POST">';
 
 foreach ($return as $var) { ?>
     <input type="checkbox" name="reseau" value="<?php echo $var['id']; ?>"><?php echo $var['nom_reseau']; ?></input><br/>
 <?php }
+
+echo '<input type="submit" value="Supprimer"></input>';
+echo '</form>';
