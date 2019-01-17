04a54930b2b38094f20b723fdb323a1268428ce6
Novaedra <leofouquier20111999@gmail.com>
2019-01-17 19:13:12 +0100

    fichier

--- reseau.php
+++ reseau.php
@@ -8,4 +8,11 @@ $sql = "SELECT * FROM reseau;";
 $query = $pdo -> prepare($sql);
 $query -> execute();
 $return = $query -> fetchALL();
+
 tab($return);
+
+echo '<form method="post">';
+
+foreach ($return as $var) {
+    echo '<input type="chechbox" name="reseau" value="'.$var[0]['id'].'">'.$var[0]['id'].': Nom du réseau : '.$var[0]['nom_reseau'].'ip : '.$var[0]['ip_saisie'].' masque : '.$var[0]['mask'].'ip - : '.$var[0]['ip_low'].' ip + : '.$var[0]['ip_high'].' date :'.$var[0]['created_at'].'</input>';
+}
