d46369a60dd452d7a082b1478b8d39ab811a1527
Novaedra <leofouquier20111999@gmail.com>
2019-01-17 16:31:43 +0100

    tout

--- reseau.php
+++ reseau.php
@@ -4,7 +4,7 @@ include ('inc/function.php');
 include ('inc/header.php');
 include ('inc/nav.php');
 
-$sql = "SELECT nom_resau,ip_saisie,mask,ip_low,ip_high,created_at FROM trame_waygit ";
+$sql = "SELECT nom_resau,ip_saisie,mask,ip_low,ip_high,created_at FROM trame_way";
 
 $req = mysqli_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysqli_error());
 while($data = mysqli_fetch_assoc($req))
@@ -13,8 +13,3 @@ while($data = mysqli_fetch_assoc($req))
     echo '<b>'.$data['nom_reseau'].' '.$data['ip_saisie'].'</b> '.$data['mask'].' '.$data['created_at'].;
     echo ' <i> '.$data['ip_low'].' '.$data['ip_high'].'</i><br>';
 }
-
-
-mysqli_close();
-
-echo $row[''];
