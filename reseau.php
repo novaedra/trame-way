ca7967ba43c0ae62bd965edb7a975f86862ad60d
Novaedra <leofouquier20111999@gmail.com>
2019-01-17 19:47:15 +0100

    reseau

--- reseau.php
+++ reseau.php
@@ -3,15 +3,11 @@ include ('inc/pdo.php');
 include ('inc/function.php');
 include ('inc/header.php');
 include ('inc/nav.php');
+?>
 
-$sql = "SELECT * FROM reseau;";
-$query = $pdo -> prepare($sql);
-$query -> execute();
-$return = $query -> fetchALL();
-tab($return);
+<form action="reseau.php" method="POST">
 
-echo '<form action="reseau.php" method="POST">';
-
+<?php
 if (!empty($_POST)) {
 
     foreach ($_POST as $key => $value) {
@@ -24,15 +20,29 @@ if (!empty($_POST)) {
             $query -> execute();
         }
     }
+
+    $sql = "SELECT * FROM reseau;";
+    $query = $pdo -> prepare($sql);
+    $query -> execute();
+    $return = $query -> fetchALL();
+    tab($return);
+
     foreach ($return as $var) { ?>
         <input type="checkbox" name="<?php echo $var['id']; ?>" value="supprime moi vite stp"><?php echo $var['nom_reseau']; ?></input><br/>
     <?php }
 }
 else {
+
+    $sql = "SELECT * FROM reseau;";
+    $query = $pdo -> prepare($sql);
+    $query -> execute();
+    $return = $query -> fetchALL();
+    tab($return);
+
     foreach ($return as $var) { ?>
         <input type="checkbox" name="<?php echo $var['id']; ?>" value="supprime moi vite stp"><?php echo $var['nom_reseau']; ?></input><br/>
     <?php }
 }
-
-echo '<input type="submit" value="Supprimer"></input>';
-echo '</form>';
+?>
+<input type="submit" value="Supprimer"></input>
+</form>
