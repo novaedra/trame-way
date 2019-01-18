736bf7ae795459e3f722fe6307a0b48791616c38
Novaedra <leofouquier20111999@gmail.com>
2019-01-18 14:15:37 +0100

    erreur

--- fichier.php
+++ fichier.php
@@ -174,7 +174,7 @@ if (!empty($protocols)) {
 
     foreach ($infraction as $key){
         foreach ($key as $cle => $value) {
-            echo '<p class="important">'.$cle . ' : ' . $value . ' connexions interdites' . '<br/>'.'</p>';
+            echo '<p class="important">'.$cle . ' : ' . $value . ' connexions interdites' . '</p>'.'<br/>';
         }
     }
 
